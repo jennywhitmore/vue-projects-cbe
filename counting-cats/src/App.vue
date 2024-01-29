ab78bde9ed5aa001c0e3a911e2d461e921bef8ab
Jenny <147312222+jennywhitmore@users.noreply.github.com>
2024-01-29 14:28:47 +0100

    adds: computed property colors

--- counting-cats/src/App.vue
+++ counting-cats/src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Count all cats by their color</h1>
+  <h2>Total cats: {{ totalCount }}</h2>
+  <AttendeeCounter
+    v-for="{ text } of attendeeCounters"
+    :key="text"
+    :text="text"
+    :counter="counter"
+  />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AttendeeCounter from "./components/AttendeeCounter.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      headline: "No need for Headline",
+      attendeeCounters: [
+        {
+          text: "black",
+          counter: 0,
+        },
+        {
+          text: "white",
+          counter: 0,
+        },
+        {
+          text: "grey",
+          counter: 0,
+        },
+        {
+          text: "orange",
+          counter: 0,
+        },
+        {
+          text: "striped",
+          counter: 0,
+        },
+        {
+          text: "pink",
+          counter: 0,
+        },
+      ],
+    };
+  },
+
+  computed: {
+    totalCount() {
+      let totalCount = 0;
+      this.attendeeCounters.forEach(({ counter }) => (totalCount += counter));
+      return totalCount;
+    },
+  },
+
   components: {
-    HelloWorld,
+    AttendeeCounter,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ counting-cats/src/components/AttendeeCounter.vue
@@ -0,0 +1,55 @@
+<template>
+  <p :style="textStyle">{{ text }}: {{ counter }}</p>
+  <button @click="increase">+</button>
+  <button @click="decrease">-</button>
+  <hr />
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+    counter: Number,
+  },
+
+  computed: {
+    textStyle() {
+      const colors = {
+        black: "black",
+        white: "silver",
+        grey: "dimgrey",
+        orange: "orangered",
+        striped: "saddlebrown",
+        pink: "deeppink",
+      };
+      return { color: colors[this.text] || "black" };
+    },
+  },
+
+  methods: {
+    increase() {
+      this.$emit("increase");
+    },
+    decrease() {
+      this.$emit("decrease");
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  color: purple;
+}
+p {
+  color: dodgerblue;
+}
+</style>
